675278aa026a9d1f7d3f06df087372a45bfd5ef0
ElenaBezro <lenyka_86@mail.ru>
2024-01-08 12:31:08 +0100

    init

---
+++ src/main/java/com/bezro/shopRESTfulAPI/ShopRESTfulApiApplication.java
@@ -0,0 +1,13 @@
+package com.bezro.shopRESTfulAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopRESTfulApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopRESTfulApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bezro/shopRESTfulAPI/ShopResTfulApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bezro.shopRESTfulAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopResTfulApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
