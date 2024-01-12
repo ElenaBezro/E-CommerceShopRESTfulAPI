7904595141cbf2523808be1ffa85189b4f22a28d
ElenaBezro <lenyka_86@mail.ru>
2024-01-12 10:06:01 +0100

    ECSRA-49 store constants in a separate class

---
+++ src/main/java/com/bezro/shopRESTfulAPI/constants/JwtConstants.java
@@ -0,0 +1,6 @@
+package com.bezro.shopRESTfulAPI.constants;
+
+public class JwtConstants {
+    public static final String AUTHORIZATION_HEADER_NAME = "Authorization";
+    public static final String AUTHORIZATION_HEADER_STARTS_WITH = "Bearer ";
+}
