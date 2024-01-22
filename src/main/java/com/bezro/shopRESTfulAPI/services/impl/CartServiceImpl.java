f01504d7d87be74549d3a775310d88235469662b
ElenaBezro <lenyka_86@mail.ru>
2024-01-22 22:11:05 +0100

    ECSRA-97 Check whether product stock is sufficient

--- src/main/java/com/bezro/shopRESTfulAPI/services/impl/CartServiceImpl.java
+++ src/main/java/com/bezro/shopRESTfulAPI/services/impl/CartServiceImpl.java
@@ -2,6 +2,7 @@ package com.bezro.shopRESTfulAPI.services.impl;
 
 import com.bezro.shopRESTfulAPI.dtos.CreateCartItemDto;
 import com.bezro.shopRESTfulAPI.entities.CartItem;
+import com.bezro.shopRESTfulAPI.entities.Product;
 import com.bezro.shopRESTfulAPI.exceptions.InvalidMethodArgumentsException;
 import com.bezro.shopRESTfulAPI.repositories.CartRepository;
 import com.bezro.shopRESTfulAPI.services.CartService;
@@ -18,11 +19,12 @@ public class CartServiceImpl implements CartService {
     private final ProductService productService;
     private final UserService userService;
 
-
     public CartItem addCartItem(CreateCartItemDto cartItemDto, Principal principal) {
-        //TODO: check if product Quantity >= cart Item Quantity
-        CartItem cartItem;
         Long productId = cartItemDto.getProductId();
+        Product product = productService.findById(productId);
+        sufficientProductStockCheck(product, cartItemDto);
+
+        CartItem cartItem;
         Long userId = cartItemDto.getUserId();
 
         if (existsByProductId(productId)) {
@@ -30,12 +32,19 @@ public class CartServiceImpl implements CartService {
             return updateCartItemQuantity(cartItem.getId(), cartItemDto, principal);
         } else {
             cartItem = new CartItem();
-            cartItem.setProduct(productService.findById(productId));
+            cartItem.setProduct(product);
             cartItem.setUser(userService.findByIdOrThrow(userId));
             userMatchPrincipalCheck(userId, principal);
             cartItem.setQuantity(cartItemDto.getQuantity());
 
             return cartRepository.save(cartItem);
+        }
+    }
+
+    private void sufficientProductStockCheck(Product product, CreateCartItemDto cartItemDto) {
+        boolean isProductStockSufficient = product.getQuantity() >= cartItemDto.getQuantity();
+        if (!isProductStockSufficient) {
+            throw new InvalidMethodArgumentsException("Product stock is not sufficient");
         }
     }
 
@@ -57,15 +66,16 @@ public class CartServiceImpl implements CartService {
     }
 
     public CartItem updateCartItemQuantity(Long id, CreateCartItemDto cartItemDto, Principal principal) {
-        //TODO: check if product Quantity >= cart Item Quantity
         Long userId = cartItemDto.getUserId();
         Long productId = cartItemDto.getProductId();
+        Product product = productService.findById(productId);
+        sufficientProductStockCheck(product, cartItemDto);
 
         CartItem cartItem = cartRepository.findById(id).orElseThrow(() ->
                 new InvalidMethodArgumentsException(String.format("Cart item with id: %d does not exist", id)));
         //TODO: use ModelMapper to map data from DTO to the entity
         //    modelMapper.map(cartItemDto, cartItem) ?
-        cartItem.setProduct(productService.findById(productId));
+        cartItem.setProduct(product);
         cartItem.setUser(userService.findByIdOrThrow(userId));
         userMatchPrincipalCheck(userId, principal);
         cartItem.setQuantity(cartItemDto.getQuantity());
