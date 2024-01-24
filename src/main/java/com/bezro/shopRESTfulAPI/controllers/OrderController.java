1bff0114f967fb25e3eaae225565e979853af8de
ElenaBezro <lenyka_86@mail.ru>
2024-01-25 00:54:06 +0100

    ECSRA-107 Create updateOrderStatus endpoint in controller

--- src/main/java/com/bezro/shopRESTfulAPI/controllers/OrderController.java
+++ src/main/java/com/bezro/shopRESTfulAPI/controllers/OrderController.java
@@ -12,9 +12,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 
@@ -41,4 +39,23 @@ public class OrderController {
         return orderService.createOrder(principal);
     }
 
+    @PutMapping("/{id}")
+    @Parameter(in = ParameterIn.HEADER,
+            description = "Authorization token",
+            name = "JWT",
+            content = @Content(schema = @Schema(type = "string")))
+    @Operation(summary = "Update order status", description = "Update order status", tags = {"Update order status"})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful operation",
+                    content = @Content(schema = @Schema(implementation = Order.class))),
+            @ApiResponse(responseCode = "400", description = "Invalid order id",
+                    content = @Content(schema = @Schema(implementation = ApiException.class))),
+            @ApiResponse(responseCode = "403", description = "Forbidden. Only administrators can access this page",
+                    content = @Content(schema = @Schema(implementation = ApiException.class))),
+            @ApiResponse(responseCode = "401", description = "User should be authenticated",
+                    content = @Content(schema = @Schema(implementation = ApiException.class)))
+    })
+    public Order updateOrderStatus(@PathVariable Long id, Principal principal) {
+        return orderService.updateOrderStatus(id, principal);
+    }
 }
--- src/main/java/com/bezro/shopRESTfulAPI/entities/OrderStatus.java
+++ src/main/java/com/bezro/shopRESTfulAPI/entities/OrderStatus.java
@@ -3,5 +3,13 @@ package com.bezro.shopRESTfulAPI.entities;
 public enum OrderStatus {
     PROCESSING,
     SHIPPED,
-    DELIVERED
+    DELIVERED;
+
+    public static OrderStatus getNext(OrderStatus previousStatus) {
+        return switch (previousStatus) {
+            case PROCESSING -> OrderStatus.SHIPPED;
+            case SHIPPED -> OrderStatus.DELIVERED;
+            default -> previousStatus;
+        };
+    }
 }
