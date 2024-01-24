cb573b87dbf13431dc9e25a6b110a317f890c8ba
ElenaBezro <lenyka_86@mail.ru>
2024-01-24 11:11:26 +0100

    ECSRA-48 Create a minimum viable OrderItems service and repository.

---
+++ src/main/java/com/bezro/shopRESTfulAPI/repositories/OrderItemRepository.java
@@ -0,0 +1,9 @@
+package com.bezro.shopRESTfulAPI.repositories;
+
+import com.bezro.shopRESTfulAPI.entities.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+}
